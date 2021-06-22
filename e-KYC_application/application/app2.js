3e47552b84b0f84b6bd4cbf0616b82c83cf68d2d
Kausthub Thekke Madathil <kausthubtm6671@gmail.com>
2021-06-22 10:01:21 +0530

    adding App2

--- e-KYC_application/application/app2.js
+++ e-KYC_application/application/app2.js
@@ -4,15 +4,15 @@ const { Gateway, Wallets } = require('fabric-network');
 const FabricCAServices = require('fabric-ca-client');
 const path = require('path');
 const { buildCAClient, registerAndEnrollUser, enrollAdmin } = require('../../test-application/javascript/CAUtil.js');
-const { buildCCPOrg1, buildWallet } = require('../../test-application/javascript/AppUtil.js');
+const { buildCCPOrg2, buildWallet } = require('../../test-application/javascript/AppUtil.js');
 const readline = require("readline");
 const prompt = require('prompt');
 
 const channelName = 'mychannel';
 const chaincodeName = 'basic';
-const mspOrg1 = 'Org1MSP';
-const walletPath = path.join(__dirname, 'wallet');
-const org1UserId = 'appUser';
+const mspOrg2 = 'Org2MSP';
+const walletPath = path.join(__dirname, 'wallet2');
+const org1UserId = 'appUser2';
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -25,21 +25,21 @@ function prettyJSONString(inputString) {
 async function main() {
 	try {
 		// build an in memory object with the network configuration (also known as a connection profile)
-		const ccp = buildCCPOrg1();
+		const ccp = buildCCPOrg2();
 
 		// build an instance of the fabric ca services client based on
 		// the information in the network configuration
-		const caClient = buildCAClient(FabricCAServices, ccp, 'ca.org1.example.com');
+		const caClient = buildCAClient(FabricCAServices, ccp, 'ca.org2.example.com');
 
 		// setup the wallet to hold the credentials of the application user
 		const wallet = await buildWallet(Wallets, walletPath);
 
 		// in a real application this would be done on an administrative flow, and only once
-		await enrollAdmin(caClient, wallet, mspOrg1);
+		await enrollAdmin(caClient, wallet, mspOrg2);
 
 		// in a real application this would be done only when a new user was required to be added
 		// and would be part of an administrative flow
-		await registerAndEnrollUser(caClient, wallet, mspOrg1, org1UserId, 'org1.department1');
+		await registerAndEnrollUser(caClient, wallet, mspOrg2, org1UserId, 'org2.department1');
 
 		// Create a new gateway instance for interacting with the fabric network.
 		// In a real application this would be done as the backend server session is setup for
